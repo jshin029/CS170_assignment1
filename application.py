6d5b91c10d6064d5d9a40f52e0eea6e6bcb56095
John Shin <jshin029@ucr.edu>
2019-11-06 02:27:51 -0800

    fixed formatting of code

--- application.py
+++ application.py
@@ -3,15 +3,6 @@ import copy
 import numpy as np
 import heapq
 
-# function general-search(problem, QUEUEING-FUNCTION)
-# nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL-STATE))
-# loop do
-#  if EMPTY(nodes) then return "failure"
-#    node = REMOVE-FRONT(nodes)
-#  if problem.GOAL-TEST(node.STATE) succeeds then return node
-#     nodes = QUEUEING-FUNCTION(nodes, EXPAND(node, problem.OPERATORS))
-#  end
-
 def uniformCostSearch(puzzle, goalState):
     col = len(puzzle[0])
     row = len(puzzle)
@@ -23,15 +14,25 @@ def uniformCostSearch(puzzle, goalState):
     puzzleQ = queue.Queue()
     puzzleQ.put(puzzle)
 
+    print "\n"
+    print "Expanding state "
+    print puzzle[0]
+    print puzzle[1]
+    print puzzle[2]
+    print "\n"
+
     while not puzzleQ.empty():
         if maxQ < puzzleQ.qsize():
             maxQ = puzzleQ.qsize()
 
-        depth += 1
-
         currentPuzzle = puzzleQ.get()
         numpPuzzle = np.array(currentPuzzle)
         findSpace = np.where(numpPuzzle==0)
+
+        if currentPuzzle == puzzle:
+            depth = 0 #if currentPuzzle is the given puzzle then the depth should be 0
+        else:
+            depth += 1
 
         index_row = findSpace[0][0]
         index_col = findSpace[1][0]
@@ -39,6 +40,11 @@ def uniformCostSearch(puzzle, goalState):
         if currentPuzzle == goalState:
             print "maximum queue size: ", maxQ
             return
+
+        print "The best state to expand with a g(n) = ", depth, " is..."
+        print currentPuzzle[0]
+        print currentPuzzle[1]
+        print currentPuzzle[2], "Expanding this node..."
 
         #check if space can move left
         if (index_col - 1) >= 0:
@@ -122,16 +128,23 @@ def aStar(puzzle, goalState, heuristic):
     F_N = heuristic(puzzle, goalState)
     heapq.heappush(heap, (F_N, depth, puzzle))
 
+    print "\n"
+    print "Expanding state "
+    print puzzle[0]
+    print puzzle[1]
+    print puzzle[2]
+    print "\n"
+
     while len(heap) != 0:
         if maxQ < len(heap):
             maxQ = len(heap)
 
         currentNode = heapq.heappop(heap)
         currentPuzzle = currentNode[2]
-        # if currentPuzzle == puzzle:
-        #     currentDepth = 0 #if the node in the queue is the root node then depth should be 0
-        # else:
-        currentDepth = currentNode[1]+1 #add 1 because we moved on to the child node so depth increases
+        if currentPuzzle == puzzle:
+            currentDepth = 0 #if the node in the queue is the root node then depth should be 0
+        else:
+            currentDepth = currentNode[1]+1 #add 1 because we moved on to the child node so depth increases
         currentH_N = heuristic(currentPuzzle, goalState)
 
         print "The best state to expand with a: g(n) = ", currentDepth, " and h(n) = ", currentH_N, " is..."
@@ -145,7 +158,6 @@ def aStar(puzzle, goalState, heuristic):
 
         index_row = findSpace[0][0]
         index_col = findSpace[1][0]
-
 
         if currentPuzzle == goalState:
             print "Goal!!"
@@ -191,79 +203,72 @@ def aStar(puzzle, goalState, heuristic):
 
         numExpansions += 1
 
-#def makePuzzle():
-    # print("Enter your puzzle, use a zero to represent the blank")
-    # row1 = input("Enter the first row, use space or tabs between numbers: ")
-    # row2 = input("Enter the second row, use space or tabs between numbers: ")
-    # row3 = input("Enter the third row, use space or tabs between numbers: ")
-
 def main():
-    # choice = input("Welcome to my 170 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own. ")
-    #
-    # if choice == 1:
-    #     diff_level = input("Please enter a difficulty level for your default puzzle")
-    #     if diff_level == 1:
-    #         print "You chose the trivial level"
-    #         puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    #     elif diff_level == 2:
-    #         print "You chose the very easy level"
-    #         puzzle = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
-    #     elif diff_level == 3:
-    #         print "You chose the easy level"
-    #         puzzle = [[1, 2, 0], [4, 5, 3], [7, 8, 6]]
-    #     elif diff_level == 4:
-    #         print "You chose the doable level"
-    #         puzzle = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
-    #     elif diff_level == 5:
-    #         print "You chose the oh boy level"
-    #         puzzle = [[8, 7, 1], [6, 0, 2], [5, 4, 3]]
+    choice = input("Welcome to my (John Shin's) 170 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own. \n")
 
-    # if choice == 2:
-    #     print "Enter your puzzle, using a zero to represent the blank. Please only enter valid 8-puzzles. Enter the puzzle delimiting the numbers with a space. RET only when finished."
-    #     first_row, second_row, third_row, puzzle = [], [], [], []
-    #     print "Enter the first row: "
-    #     index0,index1,index2=map(int,raw_input().split())
-    #     first_row.append(index0)
-    #     first_row.append(index1)
-    #     first_row.append(index2)
-    #
-    #     print "Enter the second row: "
-    #     index0,index1,index2=map(int,raw_input().split())
-    #     second_row.append(index0)
-    #     second_row.append(index1)
-    #     second_row.append(index2)
-    #
-    #     print "Enter the third row: "
-    #     index0,index1,index2=map(int,raw_input().split())
-    #     third_row.append(index0)
-    #     third_row.append(index1)
-    #     third_row.append(index2)
-    #
-    #     puzzle.append(first_row)
-    #     puzzle.append(second_row)
-    #     puzzle.append(third_row)
-    #
-    #     algorithm_choice = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic.")
+    if choice == 1:
+        diff_level = input("Please enter a difficulty level for your default puzzle from (1-5): ")
+        if diff_level == 1:
+            print "You chose the trivial level"
+            puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        elif diff_level == 2:
+            print "You chose the very easy level"
+            puzzle = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
+        elif diff_level == 3:
+            print "You chose the easy level"
+            puzzle = [[1, 2, 0], [4, 5, 3], [7, 8, 6]]
+        elif diff_level == 4:
+            print "You chose the doable level"
+            puzzle = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
+        elif diff_level == 5:
+            print "You chose the oh boy level"
+            puzzle = [[8, 7, 1], [6, 0, 2], [5, 4, 3]]
+
+    if choice == 2:
+        print "Enter your puzzle, using a zero to represent the blank. Please only enter valid 8-puzzles. Enter the puzzle delimiting the numbers with a space. RET only when finished. \n"
+        first_row, second_row, third_row, puzzle = [], [], [], []
+        print "Enter the first row: "
+        index0,index1,index2=map(int,raw_input().split())
+        first_row.append(index0)
+        first_row.append(index1)
+        first_row.append(index2)
+
+        print "Enter the second row: "
+        index0,index1,index2=map(int,raw_input().split())
+        second_row.append(index0)
+        second_row.append(index1)
+        second_row.append(index2)
+
+        print "Enter the third row: "
+        index0,index1,index2=map(int,raw_input().split())
+        third_row.append(index0)
+        third_row.append(index1)
+        third_row.append(index2)
+
+        puzzle.append(first_row)
+        puzzle.append(second_row)
+        puzzle.append(third_row)
+
+    algorithm_choice = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic. ")
+    goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+    if algorithm_choice == 1:
+        #uniform cost search
+        uniformCostSearch(puzzle, goalState)
+    elif algorithm_choice == 2:
+        #A* using missing tile heuristic
+        aStar(puzzle, goalState, MTHeuristic)
+    elif algorithm_choice == 3:
+        #A* using manhattan distance heuristic
+        aStar(puzzle, goalState, MHDHeuristic)
 
 
-    #puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    #puzzle = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
-    #puzzle = [[1, 2, 0], [4, 5, 3], [7, 8, 6]]
-    #puzzle = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
-
+    #Sample puzzles generated
     #puzzle = [[0, 1, 3], [4, 2, 5], [7, 8, 6]]
     #puzzle = [[4, 1, 3], [2, 8, 5], [7, 0, 6]]
     #puzzle = [[0, 2, 3], [1, 5, 6], [4, 7, 8]]
     #puzzle = [[1, 3, 0], [4, 2, 5], [7, 8, 6]]
     #puzzle = [[1, 2, 3], [7, 4, 5], [0, 8, 6]]
-
-    puzzle = [[1, 2, 3], [4, 8, 0], [7, 6, 5]]
-
-    #puzzle = [[8, 7, 1], [6, 0, 2], [5, 4, 3]]
-    goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    #uniformCostSearch(puzzle, goalState)
-    #aStar(puzzle, goalState, MTHeuristic)
-    aStar(puzzle, goalState, MHDHeuristic)
 
 if __name__ == "__main__":
     main()
